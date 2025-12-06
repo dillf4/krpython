8ccaaebdb71c904373a0781c2619f1e060084318
wonjunjang919 <wonjunjang919@gmail.com>
2025-12-06 11:24:33 +0900

    Update krpython.py
    deleted stupid 0 in return

--- krpython.py
+++ krpython.py
@@ -1,12 +1,12 @@
 x = 0
 def 출력하다(b): #print
     print(b)
-    return 0
+    return
 
 def 정수x(a): #int
     global x
     x = a
-    return 0
+    return
 def 입력하다(): #input()
     return input()
 
@@ -14,3 +14,4 @@ def 입력하다(): #input()
 정수x(입력하다())
 출력하다("안녕 세계!")
 출력하다(x)
+
